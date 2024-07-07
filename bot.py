775d16df8a550814afd275f779a63b242541fb6f
humoyun209 <h.ahmedov209@gmail.com>
2024-07-07 08:41:34 +0500

    Added

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import asyncio
-import logging 
+import logging
 from environs import Env
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import (Message,
-                           CallbackQuery,
-                           InlineKeyboardButton,
-                           InlineKeyboardMarkup)
+from aiogram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 from aiogram.filters import Command
 from database import DataBase
 from parsers import FunBay
@@ -16,52 +18,57 @@ env = Env()
 env.read_env()
 
 
-bot = Bot(token=env.str('TOKEN'), parse_mode='HTML')
+bot = Bot(token=env.str("TOKEN"), parse_mode="HTML")
 dp = Dispatcher()
-db = DataBase('db.sqlite3')
-fb = FunBay('https://funpay.com/lots/545/', 'data/index.html', 'data/lots.json')
+db = DataBase("db.sqlite3")
+fb = FunBay("https://funpay.com/lots/545/", "data/index.html", "data/lots.json")
 
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(levelname)s - [%(asctime)s] - %(name)s - %(message)s'
+    level=logging.INFO, format="%(levelname)s - [%(asctime)s] - %(name)s - %(message)s"
 )
 
 
-@dp.message(Command(commands=['start']))
+@dp.message(Command(commands=["start"]))
 async def process_start(message: Message):
     await fb.get_html_page()
     await fb.parse_data_to_json()
-    new_lots = await db.insert_lots()
+    new_lots = await db.insert_new_lots()
     len_new_lots = len(new_lots)
     if len_new_lots == 0:
-        await message.answer('Новые лоты не найдено')
+        await message.answer("Новые лоты не найдены")
     else:
         for i, lot in enumerate(new_lots):
-            if (i+1) % 10 == 0:
-                await asyncio.sleep(2)
-            await bot.send_message(env.str('CHANNEL_URL'), f'\n✅ Новый лот!\n{lot.description}\n💰 <b>Цена:</b> {lot.price}\n<b>Посмотреть на сайте:</b> ‼️<a href="{lot.url}">Перейти</a>‼️')
-            
+            if (i + 1) % 10 == 0:
+                await asyncio.sleep(10)
+            await bot.send_message(
+                env.str("CHANNEL_URL"),
+                f'\n✅ Новый лот!\n{lot.description}\n💰 <b>Цена:</b> {lot.price}\n<b>Посмотреть на сайте:</b> ‼️<a href="{lot.url}">Перейти</a>‼️',
+            )
 
-@dp.message(Command(commands=['admin']))
+
+@dp.message(Command(commands=["admin"]))
 async def process_admin(message: Message):
-    delete_month_back = InlineKeyboardButton(text='❌ Удалить лоты', callback_data='delete:month')
+    delete_month_back = InlineKeyboardButton(
+        text="❌ Удалить лоты", callback_data="delete:month"
+    )
     kb = InlineKeyboardMarkup(inline_keyboard=[[delete_month_back]])
-    await message.answer('Вы можете удалить все лоты, созданные месяц назад: ', reply_markup=kb)
-    
+    await message.answer(
+        "Вы можете удалить все лоты, созданные месяц назад: ", reply_markup=kb
+    )
 
-@dp.callback_query(F.data == 'delete:month')
+
+@dp.callback_query(F.data == "delete:month")
 async def delete_lots(cb: CallbackQuery):
     await db.delete_old_lots()
-    await cb.message.answer('Все лоты созданные месяц назад, удалены ✅')
+    await cb.message.answer("Все лоты, созданные неделья назад, удалены ✅")
     await cb.message.delete()
-    
 
 
 @dp.message()
 async def get_echo(message: Message):
-    await message.answer('Бот ничего не понимает')
-            
+    await message.answer("Бот ничего не понимает")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     dp.run_polling(bot)
--- database.py
+++ database.py
@@ -5,7 +5,6 @@ import aiofiles
 import aiosqlite
 
 
-
 class Lot(NamedTuple):
     url: str
     description: str
@@ -22,11 +21,11 @@ class DataBase:
             result = await f.read()
             lots = json.loads(result)
             return lots
-    
-    @staticmethod  
+
+    @staticmethod
     def str_date(date=datetime.now()):
         return datetime.strftime(date, "%Y-%m-%d %H:%M:%S")
-    
+
     @staticmethod
     def org_date(date_str: str):
         return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
@@ -34,7 +33,13 @@ class DataBase:
     async def insert_all_data(self):
         data = await self.get_lots_json()
         lots = [
-            Lot(lot.get("url"), lot.get("description"), lot.get("price"), self.str_date()) for lot in data
+            Lot(
+                lot.get("url"),
+                lot.get("description"),
+                lot.get("price"),
+                self.str_date(),
+            )
+            for lot in data
         ]
         await self._insert_lots(lots)
 
@@ -47,24 +52,29 @@ class DataBase:
     async def insert_new_lots(self) -> list[Lot]:
         lots = await self.get_lots_json()
         new_lots = [
-            Lot(url=lot.get("url"), description=lot.get("description"), price=lot.get("price"), created=self.str_date())
+            Lot(
+                url=lot.get("url"),
+                description=lot.get("description"),
+                price=lot.get("price"),
+                created=self.str_date(),
+            )
             for lot in lots
             if not await self.check_data(lot.get("url"))
         ]
         await self._insert_lots(new_lots)
         return new_lots
-    
+
     async def _insert_lots(self, lots: list[Lot]):
         async with aiosqlite.connect(self.name) as conn:
             await conn.executemany(
                 "INSERT INTO lots(url, description, price, created) VALUES(?, ?, ?, ?)",
-                [tuple(lot) for lot in lots]
+                [tuple(lot) for lot in lots],
             )
             await conn.commit()
 
     async def delete_old_lots(self):
-        month_ago = datetime.now() - timedelta(days=30)
-        
+        month_ago = datetime.now() - timedelta(days=7)
+
         async with aiosqlite.connect(self.name) as conn:
             lots_base = await conn.execute(
                 "SELECT url, description, price, created FROM lots"
@@ -73,8 +83,7 @@ class DataBase:
             old_lot_urls = []
             for lot in lots:
                 if lot.created is None or self.org_date(lot.created) < month_ago:
-                    old_lot_urls.append((lot.url, ))
-            
-            await conn.executemany('DELETE FROM lots WHERE url = ?',
-                                   old_lot_urls)
+                    old_lot_urls.append((lot.url,))
+
+            await conn.executemany("DELETE FROM lots WHERE url = ?", old_lot_urls)
             await conn.commit()
